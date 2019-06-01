0ead14887b56ea57c7db69f44c2ed0991c9217a1
dayoungles <dayoungles@gmail.com>
2019-06-01 17:08:34 +0900

    add two numbers

---
+++ src/leet/AddTwoNumbers.java
@@ -0,0 +1,55 @@
+package leet;
+
+import leet.model.ListNode;
+
+import java.util.ArrayList;
+
+public class AddTwoNumbers {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+//        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//            ListNode head = new ListNode(0);
+//            ListNode currentL1 = l1;
+//            ListNode currentL2 = l2;
+//            ListNode currentNode = head;
+//            int up = 0;
+//
+//            while(currentL1 != null || currentL2 != null) {
+//
+//                int v1 = (currentL1 != null) ? currentL1.val : 0;
+//                int v2 = (currentL2 != null) ? currentL2.val : 0;
+//
+//                int currentVal = v1 + v2 + up;
+//
+//                up = currentVal / 10;
+//                currentVal = currentVal % 10;
+//
+//                currentNode.next = new ListNode(currentVal);
+//                currentNode = currentNode.next;
+//
+//                currentL1 = (currentL1 != null) ? currentL1.next : null;
+//                currentL2 = (currentL2 != null) ? currentL2.next : null;
+//
+//
+//            }
+//            if (up != 0) {
+//                currentNode.next = new ListNode(up);
+//            }
+//
+//            return head.next;
+//        }
+
+    }
+
+
+
+}
+
