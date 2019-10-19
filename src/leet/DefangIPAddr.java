d9a503198e3965f9d46f5dba102b94b6caed6c86
dayoungles <dayoungles@gmail.com>
2019-10-19 13:46:29 +0900

    final version: use String replace

--- src/leet/DefangIPAddr.java
+++ src/leet/DefangIPAddr.java
@@ -2,17 +2,6 @@ package leet;
 
 public class DefangIPAddr {
     public String defangIPaddr(String address) {
-        String[] parsed = address.split("\\.");
-        String result = "";
-        String bridge = "[.]";
-        int count= 0;
-        for (String number : parsed) {
-            result = result + number;
-            if (count == 4){
-                break;
-            }
-            result = result + bridge;
-        }
-        return result;
+        return address.replace(".", "[.]");
     }
 }
