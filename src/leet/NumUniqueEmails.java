38e3ac36133bfc95b4b2feaf5dd0185164afbf2c
dayoungles <dayoungles@gmail.com>
2020-03-17 07:18:32 +0900

    string buffer attached

--- src/leet/NumUniqueEmails.java
+++ src/leet/NumUniqueEmails.java
@@ -7,13 +7,19 @@ public class NumUniqueEmails {
     public int numUniqueEmails(String[] emails) {
         if (emails.length < 1 ) return emails.length;
         Set<String> hset = new HashSet<>();
+        StringBuffer sb = new StringBuffer();
 
         for (String email :
                 emails) {
+            sb.delete(0, sb.length());
             String[] parts = email.split("@");
-            String domain = parts[1];
+
             String withoutPlus= parts[0].split("\\+")[0];
-            hset.add(withoutPlus.replace(".", "")+"@"+domain);
+            sb.append(withoutPlus.replace(".", ""));
+            sb.append("a");
+            sb.append(parts[1]);
+            hset.add(sb.toString());
+
         }
 
         return hset.size();
