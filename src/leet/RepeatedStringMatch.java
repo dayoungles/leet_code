967e28e680c9104e0465a040ddfb0abed578e9cb
dayoungles <dayoungles@gmail.com>
2020-03-26 07:51:46 +0900

    changed string buffer to string builder/ refer solution

--- src/leet/RepeatedStringMatch.java
+++ src/leet/RepeatedStringMatch.java
@@ -4,32 +4,38 @@ public class RepeatedStringMatch {
     public int repeatedSTringMatch(String A, String B){
 //        if(A.length() > B.length()) return -1;
         if ((A.length() == B.length()) && A.equals(B)) return 1;
+        StringBuilder sb = new StringBuilder();
+//        StringBuffer sb = new StringBuffer();
 
-        StringBuffer sb = new StringBuffer();
-        sb.append(A);
-        int count = 1;
+        int count = 0;
         while(sb.length() < B.length()){
             sb.append(A);
             count++;
         }
 
-        if(sb.length() == B.length() && sb.toString().equals(B)) {
+        if(sb.indexOf(B) != -1) {
             return count;
         }
 
-        for (int i = 0; i < 2; i++) {
-            if(sb.indexOf(B) != -1) {
-                return count;
-            }
-
-            sb.append(A);
-            count++;
-        }
-
-        if (sb.indexOf(B) == -1) {
-            return -1;
-        }
-
-        return count;
+        if(sb.append(A).indexOf(B) != -1)
+            return ++count;
+////        if(sb.length() == B.length() && sb.toString().equals(B)) {
+////            return count;
+////        }
+////
+////        for (int i = 0; i < 2; i++) {
+////            if(sb.indexOf(B) != -1) {
+////                return count;
+////            }
+////
+////            sb.append(A);
+////            count++;
+////        }
+////
+////        if (sb.indexOf(B) == -1) {
+////            return -1;
+////        }
+//
+        return -1;
     }
 }
